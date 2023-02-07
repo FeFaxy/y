29ddf4ce31a2c5283e75bd37c0934d43af613985
FeFaxy <122897219+FeFaxy@users.noreply.github.com>
2023-02-07 09:48:36 -0500

    Add files via upload

---
+++ Radius.m
@@ -0,0 +1,6 @@
+% This script calculates the area of a circle
+
+% Select a number for the radius
+radius=input('enter the value of radius=')
+% The area is calculated based on the radius selected
+area=pi*(radius^2)
