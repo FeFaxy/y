7f557fe8afd1fb82f39fff93e45a59c2d9c1429a
FeFaxy <122897219+FeFaxy@users.noreply.github.com>
2023-02-07 10:36:41 -0500

    Update Length.m

--- Length.m
+++ Length.m
@@ -1,6 +1,6 @@
-%Enter a length
-length=input('Enter Length')
-%Enter wheather that length is feet or meters
+%Prompt User to enter a length
+length=input('Enter a Length')
+%Prompt User to enter wheather that length is feet or meters
 unit=input('Enter 1 for Feet or 0 for Meters','s')
 if (unit<1)
     disp('meters')
