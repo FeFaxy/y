69d0a865c43b5ccb04a3e5a8f48fe2b4e70fadaa
FeFaxy <122897219+FeFaxy@users.noreply.github.com>
2023-02-07 10:34:35 -0500

    Add files via upload

---
+++ Length.m
@@ -0,0 +1,7 @@
+%prompts the user for a character and a number and
+%formats printing both
+
+ch=input('enter a character','s');
+num=input('enter a number:');
+fprintf('the character is ', ch)
+fprintf('the number is ', num)
